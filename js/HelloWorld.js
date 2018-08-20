d4636e01b8c3a3f3ab3ff1fe96d47dd7dc74ee8b
nxtra <nickvanhoof1@gmail.com>
2018-08-20 20:58:42 +0200

    solve ex1

---
+++ js/HelloWorld.js
@@ -0,0 +1,7 @@
+function printHelloName(person) {
+    var spanElement = document.getElementById("welcome-text");
+    spanElement.innerHTML = "Hello " + person + "!";
+}
+var person = "Nick";
+printHelloName(person);
+//# sourceMappingURL=HelloWorld.js.map
